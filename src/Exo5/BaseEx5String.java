9366caa9d1da51a7afe0aa4fc96f1c7f1ead4d34
Marty RABORD <99834828+ytram973@users.noreply.github.com>
2025-10-29 14:24:55 +0100

    feat: Exo5  String

--- src/Exo5/BaseEx5String.java
+++ src/Exo5/BaseEx5String.java
@@ -1,5 +1,46 @@
 package Exo5;
 
 public class BaseEx5String {
-    
+    public static void main(String[] args) {
+
+        String phrase = "il fait beau aujourd'hui";
+        // String phrase2 = " et il fait Froid aujourd'hui";
+        // System.out.println(phrase + phrase2);
+        // phrase += phrase2;
+        System.out.println(phrase);
+
+        String find = "beau";
+        String change = "Chaud";
+
+        if (phrase.contains(find)) {
+            phrase = phrase.replace(find, change);
+            System.out.println("Le caractère est présent !");
+            System.out.println(phrase);
+
+        } else {
+            System.out.println("Le caractère n'est pas présent !");
+        }
+
+        boolean estPalindrome = true;
+
+        // String mot = "ottO";
+        String mot = "Elu par cette crapule";
+
+        // met en miniscule la string
+        mot = mot.toLowerCase().replaceAll("\\s", "");;
+
+        for (int i = 0; i < mot.length() / 2; i++) {
+            if (mot.charAt(i) != mot.charAt(mot.length() - 1 - i)) {
+                estPalindrome = false;
+                break;
+            }
+        }
+
+        // On compare la chaîne originale et l'inverse
+        if (estPalindrome) {
+            System.out.println("'" + mot + "' est un palindrome !");
+        } else {
+            System.out.println("'" + mot + "' n'est pas un palindrome.");
+        }
+    }
 }
